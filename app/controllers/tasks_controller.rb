4029ec69bfbcddd182ec0414ada6431d0d60860e
Masato AKIZAKI <m.graceful_9411@icloud.com>
2020-08-03 06:48:56 +0900

    rename status column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   
   def index
-    @tasks = Task.all.order("status asc, date asc, created_at desc")
+    @tasks = Task.all.order("completed asc, date asc, created_at desc")
   end
   
   def new
@@ -60,7 +60,7 @@ class TasksController < ApplicationController
   private
 
     def task_params
-      params.require(:task).permit(:name, :date, :time, :memo, :status)
+      params.require(:task).permit(:name, :date, :time, :memo, :completed)
     end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_214731) do
+ActiveRecord::Schema.define(version: 2020_08_02_214525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_07_19_214731) do
     t.datetime "updated_at", null: false
     t.datetime "starts_at"
     t.datetime "ends_at"
-    t.boolean "status"
+    t.boolean "completed"
     t.date "date"
     t.integer "time"
     t.integer "project_id"
