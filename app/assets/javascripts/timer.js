9d37c1897b8a33ec385b2d977e3e0f45b3a66f75
Masato AKIZAKI <m.graceful_9411@icloud.com>
2020-08-17 07:06:57 +0900

    [wip] fix script

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,52 +1,60 @@
 document.addEventListener("turbolinks:load", function(){
-  var task_time = document.getElementById("min").innerHTML * 60;
-  var time = document.getElementById("min").innerHTML * 60;
-  var counter;
-  var min = document.getElementById("min");
-  var sec = document.getElementById("sec");
-  var start = document.getElementById("start");
-  var stop = document.getElementById("stop");
-  var reset = document.getElementById("reset");
 
-  start.onclick = function() {
-    toggle();
-    counter = setInterval(count, 1000);
-  }
+  var url = document.location.href;
 
-  stop.onclick = function() {
-    toggle();
-    clearInterval(counter);
-  }
+  if (url.includes('timer')) {
+    var task_time = document.getElementById("min").innerHTML * 60;
+    var time = document.getElementById("min").innerHTML * 60;
+    var counter;
+    var min = document.getElementById("min");
+    var sec = document.getElementById("sec");
+    var start = document.getElementById("start");
+    var stop = document.getElementById("stop");
+    var reset = document.getElementById("reset");
 
-  reset.onclick = function() {
-    time = task_time;
-    sec.innerHTML = ('00' + (time % 60)).slice(-2);
-    min.innerHTML = ('00' + (Math.floor(time / 60))).slice(-2);
-  }
-
-  function toggle() {
-    if(start.disabled) {
-      start.disabled = false;
-      stop.disabled = true;
-    } else {
-      start.disabled = true
-      stop.disabled = false;
-    }
-  }
-
-  function count() {
-    if(time === 0) {
-      sec.innerHTML = ('00' + 0).slice(-2);
-      min.innerHTML = ('00' + 0).slice(-2);
+    start.onclick = function() {
       toggle();
-      alert("時間になりました。");
+      counter = setInterval(count, 1000);
+    }
+
+    stop.onclick = function() {
+      toggle();
       clearInterval(counter);
-    } else {
-      time -= 1;
+    }
+
+    reset.onclick = function() {
+      time = task_time;
       sec.innerHTML = ('00' + (time % 60)).slice(-2);
       min.innerHTML = ('00' + (Math.floor(time / 60))).slice(-2);
-      console.log(time);
+    }
+
+    $(document).on('turbolinks:before-cache turbolinks:before-render', function() {
+      clearTimeout(counter);
+    });
+
+    function toggle() {
+      if(start.disabled) {
+        start.disabled = false;
+        stop.disabled = true;
+      } else {
+        start.disabled = true
+        stop.disabled = false;
+      }
+    }
+
+    function count() {
+      if(time === 0) {
+        sec.innerHTML = ('00' + 0).slice(-2);
+        min.innerHTML = ('00' + 0).slice(-2);
+        toggle();
+        alert("時間になりました。");
+        clearInterval(counter);
+      } else {
+        time -= 1;
+        sec.innerHTML = ('00' + (time % 60)).slice(-2);
+        min.innerHTML = ('00' + (Math.floor(time / 60))).slice(-2);
+        console.log(time);
+      }
     }
   }
-
 })
