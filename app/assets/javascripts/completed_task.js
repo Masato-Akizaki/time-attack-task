20f173e15634bbc4ff416df8aaa3cb67d0733ae9
Masato AKIZAKI <m.graceful_9411@icloud.com>
2020-09-04 06:55:56 +0900

    fix ajax render

--- app/assets/javascripts/completed_task.js
+++
@@ -1,5 +0,0 @@
-var $target = document.querySelector('.completed_tasks')
-var $button = document.querySelector('.completed_tasks_btn')
-$button.addEventListener('click', function() {
-  $target.classList.toggle('is-hidden')
-})
