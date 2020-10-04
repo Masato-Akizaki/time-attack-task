024df80b654c067ff8f83019586028bc96e29828
Masato AKIZAKI <m.graceful_9411@icloud.com>
2020-10-05 07:39:28 +0900

    add user model test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
+    name { "user" }
+    email { "user@examle.com" }
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
