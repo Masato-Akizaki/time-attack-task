0b11bc25f575392cae5750e3d1086dd1232b8fbf
Masato AKIZAKI <m.graceful_9411@icloud.com>
2020-10-13 07:33:22 +0900

    add login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :tasks do
     collection do
       get 'all'
@@ -12,5 +13,8 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users, :except => [:new, :create]
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
