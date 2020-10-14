990b569b37b7d72970efe5a33d240ec7247ae011
Masato AKIZAKI <m.graceful_9411@icloud.com>
2020-10-15 07:23:10 +0900

    add login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       redirect_to users_url
     else
       render 'new'
