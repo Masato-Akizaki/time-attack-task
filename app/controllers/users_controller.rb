2879d4d13e74207bb641fb7b77e81a2bc84552b8
Masato AKIZAKI <m.graceful_9411@icloud.com>
2020-10-21 07:14:19 +0900

    add user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action:logged_in_user, only:[:edit, :update]
+  before_action:correct_user, only:[:edit, :update]
+
   def index
     @users = User.all
   end
@@ -51,4 +54,16 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(pramas[:id])
+      redirect_to(login_url) unless current_user?(@user)
+    end
 end
