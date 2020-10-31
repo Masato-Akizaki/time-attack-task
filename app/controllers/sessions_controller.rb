dde30494a286196b23c94459979e59d4d86d7a76
Masato AKIZAKI <m.graceful_9411@icloud.com>
2020-11-01 07:02:20 +0900

    add user mailer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else 
+        message = "アカウントが有効ではありません。"
+        message += "メールを確認して登録を完了させてください。"
+        flash[:warning] = message
+        redirect_to signup_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
   
   def new
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
+      @user.send_activation_email
+      flash[:info] = "メールを送信しました。メールに記載されたURLをクリックして、登録を完了してください。"
       redirect_to users_url
     else
       render 'new'
@@ -23,6 +24,7 @@ class UsersController < ApplicationController
     
   def show
     @user = User.find(params[:id])
+    redirect_to users_url and return unless @user.activated?
   end
 
   def edit
