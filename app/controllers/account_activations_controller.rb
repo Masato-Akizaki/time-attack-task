bfb4e611468d14a95766171cce42a81dc6d3e9b3
Masato AKIZAKI <m.graceful_9411@icloud.com>
2020-11-02 07:42:15 +0900

    fix error

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,7 +2,7 @@ class AccountActivationsController < ApplicationController
 
   def edit
     user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activatin, params[:id])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
       flash[:success] = "登録が完了しました！"
@@ -10,4 +10,7 @@ class AccountActivationsController < ApplicationController
     else
       flash[:danger] = "リンクが無効です"
       redirect_to signup_url
+    end
+  end
+
 end
