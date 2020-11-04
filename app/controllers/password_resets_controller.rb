fba2c44a33c159ca433adad4f1c4f1491354551b
Masato AKIZAKI <m.graceful_9411@icloud.com>
2020-11-05 06:49:53 +0900

    add password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -17,4 +21,33 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      @user.update_attributes(:reset_digest, nil)
+      flash[:success] = "パスワードをリセットしました"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+    
+    def valid_user
+      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+      redirect_to login_url
+      end
+    end
 end
