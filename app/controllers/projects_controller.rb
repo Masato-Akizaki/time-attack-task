84477c013776f6e7ab1bcd7d05ebd02fc7d94c30
Masato AKIZAKI <m.graceful_9411@icloud.com>
2020-11-23 16:17:58 +0900

    fix tasks and projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,17 +17,17 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
     @projects = current_user.projects.all
     @tasks = current_user.tasks.where(project_id: @project.id).order("completed asc, date asc, created_at desc")
   end
 
   def edit
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
     if @project.update_attributes(project_params)
       redirect_to projects_url
     else
@@ -36,7 +36,7 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
     if @project.nil?
       redirect_to projects_url
     elsif @project.destroy
