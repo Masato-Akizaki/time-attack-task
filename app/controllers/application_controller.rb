023dfae1febb8a57801e70e542fac01c47875fdd
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-01-29 06:36:15 +0900

    set error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,24 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
 
+# 例外処理
+rescue_from Exception, with: :render_500
+rescue_from ActiveRecord::RecordNotFound, with: :render_404
+rescue_from ActionController::RoutingError, with: :render_404
+rescue_from NoMethodError, with: :render_404
+
+def render_403
+  render template: 'errors/error_403', status: 403, layout: 'application', content_type: 'text/html'
+end
+
+def render_404
+  render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
+end
+
+def render_500
+  render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
+end
+
   private
 
     def logged_in_user
